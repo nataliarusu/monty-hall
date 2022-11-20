f7ce7bd2ac8a9fe5c76eb10f0981eb5b53ca4aeb
Natalia Rusu <bonapartik@gmail.com>
2022-11-20 16:09:21 +0000

    refactor render statistics and probabilities

--- index.js
+++ index.js
@@ -1,55 +1,97 @@
-const statisticsListEl = document.querySelector('.statistics-items');
-
-const statistics=[];
-let itemsGenerated = 0;
+const statisticsList = document.querySelector('.statistics-items');
+const probabilityList = document.querySelector('.probability-items');
 
 const getRandom = (max) => {
   return Math.floor(Math.random() * max + 1);
 };
-
+/**
+ * get result of rundom matched 
+ * @param {number} count number of iteration to get statistics
+ * @param {number} doors number of doors 
+ * @returns {object} {iterations:, doors:, stick:,switch:};
+ */
 const getStatistics = (count, doors) => {
-  const result = {
-    iterations: count,
-    doors: doors,
-    sticked: 0,
-    switched: 0,
-    id: new Date().getTime().toString()+itemsGenerated
-  };
-  itemsGenerated++;
-  while (count > 0) {
-    const carDoor = getRandom(doors); //2
+  let iterate=count;
+  let sticked= 0;
+  let switched= 0;
+  while (iterate > 0) {
+    const carDoor = getRandom(doors);
     const userDoor = getRandom(doors);
     if (userDoor === carDoor) {
-      result.sticked++;
+      sticked++;
     } else {
-      result.switched++;
+      switched++;
     }
-    count--;
+    iterate--;
   }
-  return result;
+ console.log(sticked, switched)
+  return {
+    iterations: count,
+    doors: doors,
+    stick: sticked,
+    switch: switched,
+  };
 };
 
-const renderResult=(result)=>{
-    const li = createStatisticsItem(result);
-    statisticsListEl.append(li)
-}
-const startStatistics=()=>{
-    let result=getStatistics(3, 3);
-    statistics.push(result);
-    renderResult(result);
-    result=getStatistics(10, 3);
-    statistics.push(result);
-    renderResult(result);
-    result=getStatistics(100, 3);
-    statistics.push(result);
-    renderResult(result);
-    result = getStatistics(1000, 3);
-    statistics.push(result);
-    renderResult(result);
-    result = getStatistics(10000, 3);
-    statistics.push(result);
-    renderResult(result);
-    console.log(statistics)
-}
-startStatistics();
+const renderProbabilities = () => {
+  const P = [
+    [3, 1],
+    [4, 2],
+    [5, 1],
+    [5, 2],
+    [5, 3],
+  ];
 
+  for (const pData of P) {
+    const [doors, openDoors] = pData;
+    let probability = calculatePobability(doors, openDoors);
+    const li = createProbabilityItem(probability);
+    probabilityList.append(li);
+  }
+};
+const renderStatistics = () => {
+  const S = [
+    [3, 3],
+    [10, 3],
+    [100, 3],
+    [1000, 3],
+    [10000, 3],
+  ];
+
+  for (const ivData of S) {
+    //iterations, value
+    const [iterations, value] = ivData;
+    let result = getStatistics(iterations, value);
+    console.log(result)
+    const li = createStatisticsItem(result);
+    statisticsList.append(li);
+  }
+};
+
+const recalculateHandler = (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    const li = e.target.closest('li');
+    const iterations = li.dataset.count;
+    const doors = li.dataset.doors;
+    const result = getStatistics(iterations, doors);
+
+    li.querySelector('.stick .bar').style.setProperty(
+      'height',
+      `${(result.stick * 100) / result.iterations}%`
+    );
+
+    li.querySelector('.switch .bar').style.setProperty(
+      'height',
+      `${(result.switch * 100) / result.iterations}%`
+    );
+    li.querySelector(
+      '.stick-result-output'
+    ).innerHTML = `Stick and matched: ${result.stick} times`;
+    li.querySelector(
+      '.switch-result'
+    ).innerHTML = `Switch and matched: ${result.switch} times`;
+  }
+};
+renderProbabilities();
+renderStatistics();
+statisticsList.addEventListener('click', recalculateHandler);
