e167c493a8fd520494c882b248c2adc676efebdd
Natalia Rusu <bonapartik@gmail.com>
2022-11-20 16:04:51 +0000

    extract probability message and move to createListItem.js

--- src/generateProbability.js
+++ src/generateProbability.js
@@ -7,38 +7,27 @@ const openDoorslabel = document.querySelector(
 );
 const probabilityRtitle = document.querySelector('.probability-output--title');
 const probabilityGraph = document.querySelector('.probability-output--graph');
-const resultInfoOutputEl = document.querySelector('.probability-output--result');
-const probabilityList = document.querySelector('.probability-items');
+const resultInfoOutputEl = document.querySelector(
+  '.probability-output--result'
+);
 
-const generatePmessage = (result) => {
-  const infoContainerEl = document.createElement('div');
-  const stickedEl = document.createElement('p');
-  stickedEl.classList.add('stick-result-output');
-  const switchedEl = document.createElement('p');
-  switchedEl.classList.add('switch-result');
-  console.log(result.sticked * 100);
-  stickedEl.innerHTML = `Stick probability: ${result.sticked * 100}`;
-  switchedEl.innerHTML = `Switch probability: ${result.switched * 100}`;
-  infoContainerEl.append(stickedEl, switchedEl);
-  return infoContainerEl;
-};
 const displayError = (el, errClass) => {
   el.classList.add(errClass);
 };
-const renderProbability = (result, doors, openDoors) => {
-  probabilityRtitle.innerHTML='';
+const resetFormResult = () => {
+  probabilityRtitle.innerHTML = '';
   probabilityGraph.innerHTML = '';
   resultInfoOutputEl.innerHTML = '';
+};
+const renderProbability = (result, doors, openDoors) => {
+  resetFormResult();
   const stickPercent = result.sticked;
   const switchPercent = result.switched;
-  probabilityGraph.setAttribute('data-stick-probability', result.sticked);
-  console.log(stickPercent, switchPercent);
-  probabilityGraph.setAttribute('data-switch-probability', result.switched);
   const stickEl = createBar('stick', stickPercent * 100);
   const switchEl = createBar('switch', switchPercent * 100);
   probabilityGraph.append(stickEl, switchEl);
-  probabilityRtitle.innerHTML=`Probability of total ${doors} doors and ${openDoors} open doors`;
-  const resultInfoEl = generatePmessage(result);
+  probabilityRtitle.innerHTML = `Probability of total ${doors} doors and ${openDoors} open doors`;
+  const resultInfoEl = generatePobabilityMessage(result);
   resultInfoOutputEl.append(resultInfoEl);
 };
 const calculateProbabilityHandler = (e) => {
@@ -80,7 +69,6 @@ const validateOpenDoorsHanler = (e) => {
     openDoorslabel.classList.remove('error-doors');
   }
 };
-const addProbabilityItemHandler = () => {};
 
 openDoorsInput.addEventListener('input', validateOpenDoorsHanler);
 doorsInput.addEventListener('input', validateDoorsHandler);
