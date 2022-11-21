703686d20860b41f05b5e5bdf65c213458619b87
Natalia Rusu <bonapartik@gmail.com>
2022-11-21 10:58:44 +0000

    extract statistic calculation to separate file for readibility and debagging purpose

--- index.js
+++ index.js
@@ -1,48 +1,16 @@
+import { calculatePobability } from './src/calculateProbability.js';
+import { calculateStatistics } from './src/calculateStatistics.js';
+import { createProbabilityItem, createStatisticsItem} from './src/createListItem.js';
+import { generateProbabilityHandler, calculateInitialP} from './src/generateProbability.js';
+import { showHelpInfoHandler } from './src/help.js';
+import { recalculateHandler } from './src/calculateStatistics.js';
+const getHelp = document.querySelector('.get-help');
 const statisticsList = document.querySelector('.statistics-items');
 const probabilityList = document.querySelector('.probability-items');
-
-const getRandom = (max) => {
-  return Math.floor(Math.random() * max + 1);
-};
-/**
- * get the result of iterations showing the possibility that the user choice matches the car door, 
- * returns the result of a stick or switch, for Monty Hall (3 doors)  * 
- * result depends on the number of iterations, the data is for statistic purpose only
- * @param {number} count number of iteration to get statistics
- * @param {number} doors number of doors 
- * @returns {object} {iterations:, doors:, stick:,switch:};
- */
-const getStatistics = (count, doors) => {
-  let iterate=count;
-  let sticked= 0;
-  let switched= 0;
-  while (iterate > 0) {
-    const carDoor = getRandom(doors);
-    const userDoor = getRandom(doors);
-    if (userDoor === carDoor) {
-      sticked++;
-    } else {
-      switched++;
-    }
-    iterate--;
-  }
- console.log(sticked, switched)
-  return {
-    iterations: count,
-    doors: doors,
-    stick: sticked,
-    switch: switched,
-  };
-};
+const form = document.querySelector('#probability-form');
 
 const renderProbabilities = () => {
-  const P = [
-    [3, 1],
-    [4, 2],
-    [5, 1],
-    [5, 2],
-    [5, 3],
-  ];
+  const P = [[5, 1], [5, 2], [5, 3], [10]];
 
   for (const pData of P) {
     const [doors, openDoors] = pData;
@@ -53,47 +21,27 @@ const renderProbabilities = () => {
 };
 const renderStatistics = () => {
   const S = [
-    [3, 3],
     [10, 3],
     [100, 3],
     [1000, 3],
     [10000, 3],
+    [1000, 10],
+    [1000, 100],
   ];
 
   for (const ivData of S) {
     //iterations, value
     const [iterations, value] = ivData;
-    let result = getStatistics(iterations, value);
-    console.log(result)
+    let result = calculateStatistics(iterations, value);
+    console.log(result);
     const li = createStatisticsItem(result);
     statisticsList.append(li);
   }
 };
 
-const recalculateHandler = (e) => {
-  if (e.target.tagName === 'BUTTON') {
-    const li = e.target.closest('li');
-    const iterations = li.dataset.count;
-    const doors = li.dataset.doors;
-    const result = getStatistics(iterations, doors);
-
-    li.querySelector('.stick .bar').style.setProperty(
-      'height',
-      `${(result.stick * 100) / result.iterations}%`
-    );
-
-    li.querySelector('.switch .bar').style.setProperty(
-      'height',
-      `${(result.switch * 100) / result.iterations}%`
-    );
-    li.querySelector(
-      '.stick-result-output'
-    ).innerHTML = `Stick and matched: ${result.stick} times`;
-    li.querySelector(
-      '.switch-result'
-    ).innerHTML = `Switch and matched: ${result.switch} times`;
-  }
-};
+calculateInitialP();
 renderProbabilities();
 renderStatistics();
 statisticsList.addEventListener('click', recalculateHandler);
+form.addEventListener('submit', generateProbabilityHandler);
+getHelp.addEventListener('click', showHelpInfoHandler);
---
+++ src/calculateStatistics.js
@@ -0,0 +1,64 @@
+const getRandom = (max) => {
+  return Math.floor(Math.random() * max + 1);
+};
+/**
+ * calculateStatistics the result of iterations showing the possibility that the user choice matches the car door,
+ * returns the result of a stick or switch, for Monty Hall (3 doors)  *
+ * result depends on the number of iterations(games), the data is for statistic purpose only
+ * @param {number} count number of iteration to get statistics
+ * @param {number} doors number of doors
+ * @returns {object} {iterations:, doors:, stick:,switch:};
+ */
+export const calculateStatistics = (count, doors) => {
+  let iterate = count;
+  let sticked = 0;
+  let switched = 0;
+  while (iterate > 0) {
+    const carDoor = getRandom(doors);
+    const userDoor = getRandom(doors);
+    if (userDoor === carDoor) {
+      sticked++;
+    } else {
+      switched++;
+    }
+    iterate--;
+  }
+  console.log(sticked, switched);
+  return {
+    iterations: count,
+    doors: doors,
+    stick: sticked,
+    switch: switched,
+  };
+};
+
+export const recalculateHandler = (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    const li = e.target.closest('li');
+    const iterations = li.dataset.count;
+    const doors = li.dataset.doors;
+    const result = calculateStatistics(Number(iterations), Number(doors));
+    console.log((result.stick * 100) / result.iterations);
+
+    li.querySelector('.stick .bar').style.setProperty(
+      'height',
+      `${(result.stick * 100) / result.iterations}%`
+    );
+    li.querySelector('.stick .result').innerHTML = `${
+      (result.stick * 100) / result.iterations
+    }%`;
+    li.querySelector('.switch .bar').style.setProperty(
+      'height',
+      `${(result.switch * 100) / result.iterations}%`
+    );
+    li.querySelector('.switch .result').innerHTML = `${
+      (result.switch * 100) / result.iterations
+    }%`;
+    li.querySelector(
+      '.stick-result-output'
+    ).innerHTML = `Stick and matched: ${result.stick} times`;
+    li.querySelector(
+      '.switch-result'
+    ).innerHTML = `Switch and matched: ${result.switch} times`;
+  }
+};
