ef4be95693698e0abb83783e9670f3c3966f5f53
Natalia Rusu <bonapartik@gmail.com>
2022-11-21 12:13:32 +0000

    add comments to function arguments

--- src/calculateProbability.js
+++ src/calculateProbability.js
@@ -1,36 +1,25 @@
-//Note, host can open ( total doors - user open (1 door)) - (at least 1 door remains closed) )
+
+/**
+ * Note, host can open ( total doors - user open (1 door)) - (at least 1 door remains closed) )
+ * @param {number} doors number of total doors for the game
+ * @param {number} hostOpen number of how many doors host should open
+ * @returns probability object
+ * { totalDoors: doors, opened: hostOpen, sticked: probabilityPickedDoorsHasCar, switched: switchProbability };
+ */
 
 export const calculatePobability = (doors = 3, hostOpen = 1) => {
   if(doors-hostOpen <2) return;
-  // hostOpen = how many doors we want host to open, this should be at least 2 doors less than number of doors
-
-  //assuming we have 5 doors, userPickes 1 door, hostOpens = 2 doors 
-  //(host will open 2 remaining doors, 2 doors will remain closed)
-
   const pickedDoor = 1; //user picked 1 door
-  const unPickedDoors = doors - pickedDoor; // (5 - 1 = 4 doors)
-  const probabilityPickedDoorsHasCar = pickedDoor / doors; //  1/5 === 20%
-  const globalProbabilityCarBehindHost = unPickedDoors / doors; // 4/5 === 80%
-
-  //assume the car wasn't behind user's door
-  /*******************************second event probability****************************************/
-  //total doors for host is unPickedDoors, 4
-  //absolute probability for each unPickedDoor inside this event probability
-  const unPickedEachDoorProbability = 1 / unPickedDoors; //each door which unchosen by user now has equal probability=>  1/4
-
+  const unPickedDoors = doors - pickedDoor;
+  const probabilityPickedDoorsHasCar = pickedDoor / doors;
+  const globalProbabilityCarBehindHost = unPickedDoors / doors;
+  const unPickedEachDoorProbability = 1 / unPickedDoors;
   //host opens door!
-  const unopenedDoors = unPickedDoors - hostOpen; // 4 - 2 = 2 doors opened => two door remains unopened
-
-  //each unopened door ( each of 2 doors that are still closed) now have more posibility
-  // they gained after host opens the door because host opens goat doors only
-  const unopenedEachDoorProbability = 1 / unopenedDoors; // 1/2
-
+  const unopenedDoors = unPickedDoors - hostOpen;
+  const unopenedEachDoorProbability = 1 / unopenedDoors;
   const switchProbability =
     globalProbabilityCarBehindHost * unopenedEachDoorProbability;
 
   return { totalDoors: doors, opened: hostOpen, sticked: probabilityPickedDoorsHasCar, switched: switchProbability };
 };
 
-
-console.log(calculatePobability());
-console.log(calculatePobability(5, 2));
